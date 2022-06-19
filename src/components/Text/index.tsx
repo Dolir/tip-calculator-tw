1077670102c06578d47294903053cf5bfa9f7b8b
Dolir <spot1300@gmail.com>
2022-06-19 17:07:14 +0600

    refactor & adaptivity

--- src/components/Text/index.tsx
+++ src/components/Text/index.tsx
@@ -6,17 +6,24 @@ type TextProp = {
   className?: string
   size?: "small" | "normal" | "h2" | "h0"
   color?: ColorsType
+  hideOverflow?: boolean
 }
 export const Text = ({
   children,
   className = "",
   size = "normal",
-  color = "white"
+  color = "white",
+  hideOverflow = true
 }: TextProp & PropsWithChildren<unknown>) => {
   return (
     <div
       className={classNames(
-        { [className]: className, [style[size]]: size, [style[color]]: color },
+        {
+          [className]: className,
+          [style[size]]: size,
+          [style[color]]: color,
+          [style.hideOverflow]: hideOverflow
+        },
         style.text
       )}
     >
